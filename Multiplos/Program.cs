e8e8d27c96db60a6984cf169f8bdf977a0b6f9ac
Tony <antony.branquinho@gmail.com>
2023-11-30 10:08:41 -0300

    Facil

---
+++ Multiplos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos {
+    class Program {
+        static void Main(string[] args) {
+
+            int primeiro, a, b;
+
+            Console.WriteLine("Digite dois numeros inteiros: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b) {
+                primeiro = a;
+            }
+            else {
+                primeiro = b;
+            }
+
+            if (primeiro % 2 == 0) {
+                Console.WriteLine("Sao multiplos.");
+            }
+            else {
+                Console.WriteLine("Nao sao multiplos.");
+            }
+        }
+    }
+}
